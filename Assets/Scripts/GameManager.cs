dfc4afc514e90d7e8e0a1f8b5ce0bb5bb20f9e4c
Arthur Pai <baidragoon@gmail.com>
2021-11-27 17:40:23 +0800

    feat: Join Game Room

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,63 @@
+using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+namespace Tanks
 {
-  public static GameManager instance;
-
-  void Awake()
+  public class GameManager : MonoBehaviourPunCallbacks
   {
-    if (instance != null)
+    public static GameManager instance;
+
+    string gameVersion = "1";
+
+    void Awake()
     {
-      Debug.LogErrorFormat(gameObject, "Multiple instances of {0} is not allow", GetType().Name);
-      DestroyImmediate(gameObject);
-      return;
+      if (instance != null)
+      {
+        Debug.LogErrorFormat(gameObject, "Multiple instances of {0} is not allow", GetType().Name);
+        DestroyImmediate(gameObject);
+        return;
+      }
+
+      PhotonNetwork.AutomaticallySyncScene = true;
+      DontDestroyOnLoad(gameObject);
+      instance = this;
     }
 
-    DontDestroyOnLoad(gameObject);
-    instance = this;
+    void Start()
+    {
+      PhotonNetwork.ConnectUsingSettings();
+      PhotonNetwork.GameVersion = gameVersion;
+    }
+
+    public override void OnConnected()
+    {
+      Debug.Log("PUN Connected");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+      Debug.Log("PUN Connected to Master");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+      Debug.LogWarningFormat("PUN Disconnected was called by PUN with reason {0}", cause);
+    }
+    
+    public void JoinGameRoom()
+    {
+      var options = new RoomOptions
+      {
+        MaxPlayers = 6
+      };
+
+      PhotonNetwork.JoinOrCreateRoom("Kingdom", options, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+      Debug.Log("Joined room!!");
+    }
   }
 }
