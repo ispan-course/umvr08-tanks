a4e7a4975e0a51266a73a66f5937876fe5d4cba6
Arthur Pai <baidragoon@gmail.com>
2021-11-28 15:39:12 +0800

    feat: setup main menu

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Tanks
+{
+  public class HUD : MonoBehaviour
+  {
+    static HUD instance;
+
+    void Awake()
+    {
+      if (instance != null)
+      {
+        DestroyImmediate(gameObject);
+        return;
+      }
+
+      instance = this;
+      DontDestroyOnLoad(gameObject);
+    }
+  }
+}
